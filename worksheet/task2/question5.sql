7d61f7b37ef22a9f2ca021a23b92e5efb259db31
djbn0594 <djbn0594@leeds.ac.uk>
2026-02-07 15:20:29 +0000

    portfolios2w1

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,13 +1,9 @@
 -- For each student, calculate the total number of credits from courses they passed. Assume a passing grade is 40 or higher.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, TotalCreditsPassed
-SELECT S.StudentId, S.FirstName, S.LastName, 
-    COALESCE(
-    (
-        SELECT SUM(C.Credits)
-        FROM Enrolment E, Course C
-        WHERE E.CourseId = C.CourseId
-        AND E.StudentId = S.StudentId
-        AND E.Grade >= 40
-    ), 0) AS TotalCreditsPassed
+SELECT S.StudentId, S.FirstName, S.LastName, SUM(C.Credits) AS TotalCreditsPassed
 FROM Student S
+JOIN Enrolment E ON S.StudentId = E.StudentId
+JOIN Course C ON E.CourseId = C.CourseId
+WHERE E.Grade >= 40
+GROUP BY S.StudentId, S.FirstName, S.LastName
